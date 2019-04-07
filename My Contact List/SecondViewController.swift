f5a9ff83cb6c2f7adbf2d9f7829866c9b1ea131a
Cistudent <cistudent@CISs-Mac.local>
2019-04-07 12:49:54 -0700

    Initial Commit

---
+++ My Contact List/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  My Contact List
+//
+//  Created by Cistudent on 4/7/19.
+//  Copyright © 2019 Learning Mobile Apps. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
